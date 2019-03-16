1343282e5829d74142ce1710b65e753befdb67bc
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-03-16 23:26:54 +0200

    Other: fix rollup warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
+import autoExternal from 'rollup-plugin-auto-external';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
@@ -10,20 +12,21 @@ export default {
     { file: pkg.main, format: 'cjs' },
   ],
   plugins: [
+    autoExternal(),
     postcss({
       modules: true,
       extract: true,
       sourceMap: true,
     }),
+    commonjs({
+      extensions: ['.js', '.mjs'],
+    }),
     resolve({
-      customResolveOptions: {
-        moduleDirectory: 'src',
-      },
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'],
     }),
     babel({
       exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'],
     }),
   ],
 };
