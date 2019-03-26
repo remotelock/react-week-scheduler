b2c185e125ade525a1cb66cb6485d2e0a41953ec
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-03-26 17:02:06 +0200

    Feature: export DefaultEventRootComponent for composability

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
+import { DefaultEventRootComponent } from './components/DefaultEventRootComponent';
 import { TimeGridScheduler } from './components/TimeGridScheduler';
 import { SchedulerContext } from './context';
 import { useMousetrap } from './hooks/useMousetrap';
 import { classes } from './styles';
 
-export { TimeGridScheduler, classes, useMousetrap, SchedulerContext };
+export {
+  TimeGridScheduler,
+  classes,
+  DefaultEventRootComponent,
+  useMousetrap,
+  SchedulerContext,
+};
